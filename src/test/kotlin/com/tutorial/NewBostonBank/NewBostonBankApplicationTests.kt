ad387068cc61fcec2599d80dd82c0050197ba782
Juan Manual Diaz <jdiaz@bravurasolutions.com>
2022-11-11 18:11:17 +1100

    Initial commit

---
+++ src/test/kotlin/com/tutorial/NewBostonBank/NewBostonBankApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.tutorial.NewBostonBank
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NewBostonBankApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
